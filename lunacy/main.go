22c32b990af03ef4d0f0e1d5e1147c0aeb33e8a0
Alexys Lozada <alexyslozada@msn.com>
2016-08-19 14:18:02 -0500

    No es necesario importar el paquete connection
    En el archivo main.go de la carpeta lunacy se
    hace un cambio que demuestra que no se necesita del
    paquete connection

--- lunacy/main.go
+++ lunacy/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 
-	_ "github.com/golang-es/go-cms/connection"
 	"github.com/golang-es/go-cms/models"
 )
 
 func main() {
-	rol := models.Rol{ID: 3}
+	rol := models.Rol{ID: 4}
 	rol.GetByID()
 	fmt.Println(rol)
 }
