b3fe9dedef54015c3376bce94977f657998e7e15
Daniel Romero <danielromeroauk@gmail.com>
2016-10-09 20:57:20 -0500

    Se da formato al código y se corrige un if.

--- common/util.go
+++ common/util.go
@@ -1,33 +1,34 @@
 package common
 
 import (
-	"net/http"
-	"log"
 	"encoding/json"
+	"log"
+	"net/http"
 )
 
-type(
+type (
 	appError struct {
-		Error string `json:"error"`
-		Message string `json:"message"`
-		HttpStatus int `json:"status"`
+		Error      string `json:"error"`
+		Message    string `json:"message"`
+		HttpStatus int    `json:"status"`
 	}
 	errorResource struct {
 		Data appError `json:"data"`
 	}
 )
+
 //TODO: DisplayAppError - En estos momentos cuando ocurre un error la app devuelve un json que indica el error, se debe cambiar para que de una respuesta HTML.
-func DisplayAppError(w http.ResponseWriter, handlerError error, message string, code int)  {
+func DisplayAppError(w http.ResponseWriter, handlerError error, message string, code int) {
 	errObj := appError{
-		Error: handlerError.Error(),
-		Message: message,
+		Error:      handlerError.Error(),
+		Message:    message,
 		HttpStatus: code,
 	}
 
 	log.Printf("[AppError]: %s\n", handlerError)
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
-	if j, err := json.Marshal(errorResource{Data: errObj}); err {
+	if j, err := json.Marshal(errorResource{Data: errObj}); err != nil {
 		w.Write(j)
 	}
 
